cee4694259f30eaa00f50966210c5daff25ba311
Marcos Castro <marcosmax.5@gmail.com>
2022-12-09 18:11:00 -0300

    Ajustando o cabeçalho

---
+++ src/components/nav.jsx
@@ -0,0 +1,20 @@
+import logo from '../logo.svg';
+
+function Barra() {
+    return(
+      <header className="barra">
+          <a href="http://www.hogwartslegacy.com/pt-br" /*role="menuitem" aria-label="Home Page" class=""*/>
+            <img src={logo} className="nav-logo" alt="logo"/>
+          </a>
+      <nav /*id='MainNav' role={"menubar"}*/ className="barra-menu">
+          <ul id='menu'>
+           <a className="barra-menu-item"/*href="#L384" role="menuitem" aria-label="Home Page"*/>Section 1 </a>
+           <a className="barra-menu-item"/*href="#details" role="menuitem" aria-label="Home Page"*/>Section 2</a>
+           <a className="barra-menu-item"/*href="#FAQ" role="menuitem" aria-label="Home Page"*/>Section 3</a>
+         </ul>  
+      </nav>
+      </header>
+    )
+}
+
+export default Barra;
