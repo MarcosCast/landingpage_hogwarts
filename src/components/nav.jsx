e8a665ea1d3f0501e6649032fcfceaf363ec58ab
Marcos Castro <marcosmax.5@gmail.com>
2022-12-11 16:26:38 -0300

    Implementando Rotas

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -3,19 +3,21 @@ import logo from '../logo.svg';
 function Barra() {
     return(
       <header id="id-barra" className="barra">
-          <a href="http://www.hogwartslegacy.com/pt-br" /*role="menuitem" aria-label="Home Page" class=""*/>
-            <img src={logo} className="nav-logo" alt="logo"/>
+          <a href="index.html" /*role="menuitem" aria-label="Home Page" */>
+            <img src={logo} className="nav-logo" alt="Logo Hogwarts Legacy"/>
           </a>
-      <nav /*id='MainNav' role={"menubar"}*/ className="barra-menu">
+
+      <nav id='MainNav' /*data-toggle="collapse" data-target=".nav-collapse"*/ className="barra-menu">
           <ul id='menu'>
-           <a className="barra-menu-item"href="#L384" /*role="menuitem" aria-label="Home Page"*/> 
+           <a className="barra-menu-item" href="#123" role="menuitem" aria-label="Home Page"> 
            Area 1 </a>
-           <a className="barra-menu-item" href="#details" /*role="menuitem" aria-label="Home Page"*/> 
+           <a className="barra-menu-item" href="#details" role="menuitem" aria-label="Home Page"> 
            Area 2 </a>
-           <a className="barra-menu-item" href="#FAQ"/* role="menuitem" aria-label="Home Page"*/> 
+           <a className="barra-menu-item" href="#FAQ" role="menuitem" aria-label="Home Page"> 
            Area 3 </a>
          </ul>  
       </nav>
+        
       </header>
     )
 }
---
+++ src/pages/v1.jsx
@@ -0,0 +1,9 @@
+function V1() {
+    return(
+      <header id="id-barra" className="barra">
+          <p>V1</p>
+      </header>
+    )
+}
+
+export default V1;
