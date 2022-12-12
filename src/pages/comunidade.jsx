f3d206d7722ece91936b63fdb89329bbde6de78f
Marcos Castro <marcosmax.5@gmail.com>
2022-12-11 22:11:29 -0300

    Ajustanto as pages

--- src/pages/comunidade.jsx
+++ src/pages/comunidade.jsx
@@ -1,6 +1,11 @@
 import Barra from '../components/nav';
 import Rodape from '../components/footer';
 
+import Discord from '../images/discord.png';
+import Youtube from '../images/youtube.png';
+import Twitter from '../images/twitter.png';
+import Instagram from '../images/instagram.png';
+
 
 
 function Comunidade() {
@@ -8,6 +13,40 @@ function Comunidade() {
         <div className="App">      
         <Barra />   
          <main>  
+         <div class="center">
+            <div class="center-prime">
+                <div class="center-prime-texto-titulo">
+                    <p class="title nome">Comunidade</p>
+                    <figure class="image">
+                    </figure>
+                    <p class="center-prime-texto-subtitulo">Acesse nossas redes</p>
+                </div>
+                <div class="center-corpo-comu">
+                    <p class="center-prime-texto-titulo">Redes Sociais</p>
+                    <a href="https://discord.com/invite/HogwartsLegacy" class="box" id="anchors">
+                        <img src={Discord} class="rede-img"/>
+                        <p>Entre na nossa comunidade no Discord</p>
+                    </a>
+                    <a href="https://www.youtube.com/HogwartsLegacy" class="box" id="anchors">
+                        <img src={Youtube} class="rede-img"/>
+                        <p>Veja todos os vídeos incríveis no YouTube</p>
+                    </a>
+                    <a href="https://twitter.com/HogwartsLegacy" class="box" id="anchors">
+                        <img src={Twitter} class="rede-img"/>
+                        <p>Não esqueça de nos seguir</p>
+                    </a>
+                    <a href="https://www.instagram.com/devparadev/" class="box" id="anchors">
+                        <img src={Instagram} class="rede-img"/>
+                        <p>Nos acompanhe no Instagram</p>
+                    </a>
+                    <p><b>Baixe esses lindos Wallpappers</b></p>
+                    <button class="center-prime-texto-botao" type='button'>
+                    <a href='https://cdn-hogwartslegacy.warnerbrosgames.com/community/downloads/wallpapers.zip'>
+                      Download</a></button>
+                    
+                </div>
+        </div>
+    </div>
   
            </main>
        <Rodape />
