da5818b4fbcb9b911ce7af217bcc7869f7d021a4
MarcosCast <marcosmax.5@gmail.com>
2023-01-14 17:37:34 -0300

    Ajustando a barra de navegação

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -9,14 +9,13 @@ function Barra() {
             <Link to="/"><img src={Hero} className="nav-logo" alt="Logo Hogwarts Legacy"/></Link>
           </a>
 
-      <nav id='MainNav' /*data-toggle="collapse" data-target=".nav-collapse"*/ className="barra-menu">
+          <nav id='MainNav' /*data-toggle="collapse" data-target=".nav-collapse"*/ className="barra-menu">
           <ul id='menu'>
            <a className="barra-menu-item" href='Sobre' ><Link to="/sobre"> Sobre </Link></a>
            <a className="barra-menu-item" href='Midia' ><Link to="/midia"> Mídia </Link></a>
            <a className="barra-menu-item" href='Comunidade' ><Link to="/comunidade"> Comunidade </Link></a>
          </ul>    
       </nav>
-        
       </header>
     )
 }
